baeb4aafa7c8984e3ace7db8ae7e179f2de841b3
Mateus Cristófori <mateuscristofori.gomes@gmail.com>
2023-07-14 16:40:09 -0500

    Feat: Adding a image um dropdown menu (in progress).

--- src/Components/NavBar/DynamicNavLinks/DynamicHeaderWithAuthenticated.tsx
+++ src/Components/NavBar/DynamicNavLinks/DynamicHeaderWithAuthenticated.tsx
@@ -1,9 +1,6 @@
-import { Avatar } from "@mui/material";
-import { Link } from "react-router-dom";
 import useAuth from "../../../Context/hooks/useAuth";
-import avatar_icone from "../../../assets/avatar_icone.png";
-import { userAuthenticatedLinks } from "./links/userAuthenticatedLinks";
 import MenuItems from "./Menu_Items/MenuItems";
+import { userAuthenticatedLinks } from "./links/userAuthenticatedLinks";
 
 export default function DynamicHeaderWithAuthenticatedUser() {
   const { handleLogout } = useAuth();
@@ -13,11 +10,6 @@ export default function DynamicHeaderWithAuthenticatedUser() {
       {userAuthenticatedLinks.map((link) => {
         return <MenuItems items={link} />;
       })}
-      {/* <Avatar
-        alt="Ícone de avatar"
-        src={avatar_icone}
-        style={{ width: 30, height: 30 }}
-      /> */}
     </>
   );
 }
