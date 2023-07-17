bf176bce976d322593c947c431e22a3e4118711c
CRomero19 <ronerodocarmojunior@hotmail.com>
2023-07-17 13:21:16 -0300

    feat:regex-password

--- src/Components/NavBar/index.tsx
+++ src/Components/NavBar/index.tsx
@@ -10,10 +10,6 @@ export const NavBar = () => {
     if (!user) {
       return <DynamicHeaderWithUnauthenticatedUser />;
     }
-    /* NO MOMENTO NÃO EXISTEM USERS PREMIUM. REFERENCIAR AQUI O HEADER DOS MESMOS SE VIEREM A EXISTIR*/
-    if (user && user.isPremium) {
-      return <DynamicHeaderWithAuthenticatedUser />;
-    }
     if (user) {
       return <DynamicHeaderWithAuthenticatedUser />;
     }
