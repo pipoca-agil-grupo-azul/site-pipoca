80e99498e1a8b617f4c33b1a08f2a0463d67a9a6
Mateus Cristófori <mateuscristofori.gomes@gmail.com>
2023-07-20 08:32:04 -0500

    Fix: Removing button login with LinkedIn

--- src/Components/LoginWithSocialMedia/index.tsx
+++ src/Components/LoginWithSocialMedia/index.tsx
@@ -1,19 +1,13 @@
+import iconGoogleColored from "../../assets/icon_google-colored.svg";
 import { StyledLoginWithSocialMedia } from "./style";
-import iconGoogleColored from "../../assets/icon_google-colored.svg"
-import iconLkdWhite from "../../assets/icon_lkd-white.svg"
-
 
 export const LoginWithSocialMedia = () => {
   return (
     <StyledLoginWithSocialMedia>
-    <button className="btn-google-login">
+      <button className="btn-google-login">
         <img src={iconGoogleColored} alt="icon-google" />
         Entre com o Google
-    </button>
-    <button className="btn-linkedin-login" >
-        <img src={iconLkdWhite} alt="icon-google" />
-        Entre com o Linkedin
-    </button>   
+      </button>
     </StyledLoginWithSocialMedia>
   );
 };
